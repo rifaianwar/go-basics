434b55d9061fed8ab8482379f8cd2f6e253842e7
rifaianwar <rifaianwar@outlook.com>
2024-08-14 14:28:24 +0700

    Assignment Day 1

--- assignments/main.go
+++ assignments/main.go
@@ -1,25 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func main() {
 	fmt.Println("hello world")
 
-	// arraySign([]int{2, 1})                    // 1
-	// arraySign([]int{-2, 1})                   // -1
-	// arraySign([]int{-1, -2, -3, -4, 3, 2, 1}) // 1
+	arraySign([]int{2, 1})                    // 1
+	arraySign([]int{-2, 1})                   // -1
+	arraySign([]int{-1, -2, -3, -4, 3, 2, 1}) // 1
 
-	// isAnagram("anak", "kana") // true
-	// isAnagram("anak", "mana") // false
-	// isAnagram("anagram", "managra") // true
+	isAnagram("anak", "kana")       // true
+	isAnagram("anak", "mana")       // false
+	isAnagram("anagram", "managra") // true
 
-	// findTheDifference("abcd", "abcde") // 'e'
-	// findTheDifference("abcd", "abced") // 'e'
-	// findTheDifference("", "y")         // 'y'
+	findTheDifference("abcd", "abcde") // 'e'
+	findTheDifference("abcd", "abced") // 'e'
+	findTheDifference("", "y")         // 'y'
 
-	// canMakeArithmeticProgression([]int{1, 5, 3})    // true; 1, 3, 5 adalah baris aritmatik +2
-	// canMakeArithmeticProgression([]int{5, 1, 9})    // true; 9, 5, 1 adalah baris aritmatik -4
-	// canMakeArithmeticProgression([]int{1, 2, 4, 8}) // false; 1, 2, 4, 8 bukan baris aritmatik, melainkan geometrik x2
+	canMakeArithmeticProgression([]int{1, 5, 3})    // true; 1, 3, 5 adalah baris aritmatik +2
+	canMakeArithmeticProgression([]int{5, 1, 9})    // true; 9, 5, 1 adalah baris aritmatik -4
+	canMakeArithmeticProgression([]int{1, 2, 4, 8}) // false; 1, 2, 4, 8 bukan baris aritmatik, melainkan geometrik x2
 
 	// tesDeck()
 }
@@ -27,28 +31,86 @@ func main() {
 // https://leetcode.com/problems/sign-of-the-product-of-an-array
 func arraySign(nums []int) int {
 	// write code here
-
-	return 1 // if positive
+	var result int
+	sum := 1
+	for i := 0; i < len(nums); i++ {
+		sum *= nums[i]
+		if sum > 0 {
+			result = 1
+		}
+		if sum < 0 {
+			result = -1
+		}
+		if sum == 0 {
+			result = 0
+		}
+	}
+	fmt.Println("SUM:", sum)
+	fmt.Println("Result:", result)
+	return result
+	//return 1 // if positive
 	// return -1 // if negative
 }
 
 // https://leetcode.com/problems/valid-anagram
 func isAnagram(s string, t string) bool {
 	// write code here
-	return false
+	var result bool
+	runes1 := []rune(s)
+	runes2 := []rune(t)
+	sort.Slice(runes1, func(i, j int) bool { return runes1[i] < runes1[j] })
+	sort.Slice(runes2, func(i, j int) bool { return runes2[i] < runes2[j] })
+
+	if len(s) != len(t) {
+		result = false
+	}
+	if (string(runes1)) == string(runes2) {
+		result = true
+	}
+	fmt.Println(result)
+	return result
 }
 
 // https://leetcode.com/problems/find-the-difference
 func findTheDifference(s string, t string) byte {
 	// write code here
-	b := byte('a')
-	return b
+	var result byte
+	runes2 := []rune(t)
+
+	for i := 0; i < len(t); i++ {
+		if strings.Contains(s, string(runes2[i])) {
+
+		} else {
+			result = byte(runes2[i])
+		}
+	}
+	fmt.Println(string(result))
+	return result
 }
 
 // https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence
 func canMakeArithmeticProgression(arr []int) bool {
 	// write code here
-	return false
+	var temp int
+	var result bool
+	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
+	swap := arr[1] - arr[0]
+	for i := 1; i < len(arr); i++ {
+		temp = arr[i] - arr[i-1]
+
+		if temp == swap {
+			swap = temp
+			if (arr[i-1] + temp) == arr[i] {
+				result = true
+			} else {
+				result = false
+			}
+		} else {
+			result = false
+		}
+	}
+	fmt.Println("Result: ", result)
+	return result
 }
 
 // Deck represent "standard" deck consist of 52 cards
